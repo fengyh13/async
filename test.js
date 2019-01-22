c0614acca550c839d95ff1443a9ba072d3c74687
冯艳辉 <fengyh13@163.com>
2019-01-22 13:07:50 +0800

    no message

--- test.js
+++ test.js
@@ -1,63 +1,29 @@
 const Async = require('./index');
 
-// Async.waterfall([
-//   function a(cb) {
-//     setTimeout(() => {
-//       // cb(null, 2000);
-//       cb('err...')
-//     }, 1000);
-//   }, function b(data, cb) {
-//     setTimeout(() => {
-//       cb(null, data + 3000);
-//     }, 1000);
-//   }
-// ], function (err, data) {
-//   console.log(err, data)
-// })
-
-// Async.each([1, 2], function (item, cb) {
-//   setTimeout(() => {
-//     if (2 === item) {
-//       cb('err');
-//     } else {
-//       console.log(item);
-//       cb();
-//     }
-//   }, 1000);
-// }, function (err) {
-//   console.log(err)
-// })
-
-
-// var scope = "global scope";
-// function checkscope(){
-//     var scope = "local scope";
-//     function f(){
-//         return scope;
-//     }
-//     return f;
-// }
-
-// var foo = checkscope();
-// foo();
-
-
-let buf = ['index.js', 'test.js', 'a.js']
-// for (let item of buf) {
-//   cb(item);
-// }
-const fs = require('fs');
-
-for (var i = 0; i < buf.length; i++) {
-  // cb(i);
-  (function a(item) {
+Async.waterfall([
+  function a(cb) {
     setTimeout(() => {
-      console.log(item);
+      cb(null, 2000);
+      // cb('err...')
     }, 1000);
-  })(i)
-}
-function cb(i) {
+  }, function b(data, cb) {
+    setTimeout(() => {
+      cb(null, data + 3000);
+    }, 1000);
+  }
+], function (err, data) {
+  console.log(err, data)
+})
+
+Async.each([1, 2], function (item, cb) {
   setTimeout(() => {
-    console.log(i);
+    if (2 === item) {
+      cb('err');
+    } else {
+      console.log(item);
+      cb();
+    }
   }, 1000);
-}
+}, function (err) {
+  console.log(err)
+})
